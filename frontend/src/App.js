c2ff366fee36723888b2c4bcb5d3306f648efcd4
charlotte-lauren <charlotte.green@tuks.co.za>
2024-11-03 16:45:42 +0200

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
 //Charlotte Green u21434965
-import React from "react";
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Splash from './pages/Splash';
+import Landing from './pages/Landing';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Friends from './pages/Friends';
+import Playlist from './pages/Playlist';
+import SongLibrary from './pages/SongLibrary';
+import Notifications from './pages/Notifications';
+import Header from './components/Header';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // this.state should be written as a member within the constructor
-    this.state = {
-  };
-  }
-
-  render() {
-    return (
-      <div className="container">
-        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="app">
+                    <Header /> {/* Display the header on all pages */}
+                    <Switch>
+                        <Route exact path="/" component={Splash} />
+                        <Route path="/landing" component={Landing} />
+                        <Route path="/home" component={Home} />
+                        <Route path="/profile" component={Profile} />
+                        <Route path="/friends" component={Friends} />
+                        <Route path="/playlist" component={Playlist} />
+                        <Route path="/song-library" component={SongLibrary} />
+                        <Route path="/notifications" component={Notifications} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
